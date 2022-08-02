48f110cd41a049cd6b843a2d4c24b930b53645e9
murata0531 <coless115@gmail.com>
2022-08-02 23:26:40 +0900

    repair

--- src/app/resources/views/welcome.blade.php
+++ src/app/resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                {{$use}}
+                                Sponsor
                             </a>
                         </div>
                     </div>
